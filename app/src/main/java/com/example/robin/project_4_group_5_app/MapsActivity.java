2db70b20c452b097d630e8115cbb124ae7cb215f
Jeroen Meijer <0916174@hr.nl>
2016-07-06 15:07:38 +0200

    Current position works, only containers in radius of current location are drawn

--- app/src/main/java/com/example/robin/project_4_group_5_app/MapsActivity.java
+++ app/src/main/java/com/example/robin/project_4_group_5_app/MapsActivity.java
@@ -1,8 +1,12 @@
 package com.example.robin.project_4_group_5_app;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -16,11 +20,13 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private MainActivity mainActivity;
+    LocationManager mLocationManager;
 
     ArrayList<String> latContainers;
     ArrayList<String> lngContainers;
@@ -45,13 +51,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
-
         mMap.setMyLocationEnabled(true);
-        String currentLat =  Double.toString(mMap.getMyLocation().getLatitude());
-        String currentLng =  Double.toString(mMap.getMyLocation().getLongitude());
+
+        Location myLocation = getLastKnownLocation();
+
+        String currentLat = Double.toString(myLocation.getLatitude());
+        String currentLng = Double.toString(myLocation.getLongitude());
 
         //make jsonString for parsing
-        String jsonString = "10&val=0.1&lat="+currentLat+"&lng="+currentLng;
+        String jsonString = "10&val=1" +
+                "&lat=" + currentLat + "&lng=" + currentLng;
 
         //get list of coordinates of bike container
         ArrayList<ArrayList<android.util.Pair<String, String>>> lnglatContainers;
@@ -63,7 +72,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         ArrayList<String> lngContainers = new ArrayList<>();
 
         //fill string lists
-        for (ArrayList<android.util.Pair<String, String>> row:lnglatContainers) {
+        for (ArrayList<android.util.Pair<String, String>> row : lnglatContainers) {
             latContainers.add(row.get(1).second);
             lngContainers.add(row.get(2).second);
         }
@@ -72,5 +81,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         for (int i = 0; i < lngContainers.size(); i++) {
             mMap.addMarker(new MarkerOptions().position(new LatLng(Double.parseDouble(latContainers.get(i)), Double.parseDouble(lngContainers.get(i)))).title("Bike Container"));
         }
+    }
+
+
+
+    private Location getLastKnownLocation() {
+        mLocationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+        List<String> providers = mLocationManager.getProviders(true);
+        Location bestLocation = null;
+        for (String provider : providers) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            }
+            Location l = mLocationManager.getLastKnownLocation(provider);
+            if (l == null) {
+                continue;
+            }
+            if (bestLocation == null || l.getAccuracy() < bestLocation.getAccuracy()) {
+                // Found best last known location: %s", l);
+                bestLocation = l;
+            }
+        }
+        return bestLocation;
     }
 }
